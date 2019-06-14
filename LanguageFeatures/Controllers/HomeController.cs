7028ef9164b0937f4ac10f4dabdabefc80e93477
Al Pa Ho <petrov51400@gmail.com>
2019-06-14 13:47:18 +0300

    First commit and Add 3 Proj-s

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ViewResult> Index()
+        {
+            long? length = await MyASyncMethods.GetPageLength();
+
+            return View(new string[] { $"Length: {length}" });
+        }
+    }
+}
