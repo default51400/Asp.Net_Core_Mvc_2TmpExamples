fe7b4a4f0a484441904d5d45be533e65a28800ff
Al Pa Ho <petrov51400@gmail.com>
2019-06-15 02:23:42 +0300

    Add moq, check and use It

--- WorkingWithVisualStudio.Tests/HomeContollerTests.cs
+++ WorkingWithVisualStudio.Tests/HomeContollerTests.cs
@@ -4,6 +4,7 @@ using System.Text;
 using WorkingWithVisualStudio.Controllers;
 using WorkingWithVisualStudio.Models;
 using Xunit;
+using Moq;
 
 
 namespace WorkingWithVisualStudio.Tests
@@ -24,7 +25,10 @@ namespace WorkingWithVisualStudio.Tests
         [ClassData(typeof(ProductTestData))]
         public void IndexActionModelIsComplete(Product[] products)
         {
-            var controller = new HomeController();
+            var mock = new Mock<IRepository>();
+            mock.SetupGet(m => m.Products).Returns(products);
+            var controller = new HomeController() { Repository = mock.Object };
+
             controller.Repository = new ModelCompleteFakeRepository
             {
                 Products = products
@@ -33,6 +37,34 @@ namespace WorkingWithVisualStudio.Tests
             var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
 
             Assert.Equal(controller.Repository.Products, model, Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
+        }
+
+        class PropertyOnceFakeRepository : IRepository
+        {
+            public int PropertyCounter { get; set; } = 0;
+            public IEnumerable<Product> Products
+            {
+                get
+                {
+                    PropertyCounter++;
+                    return new[] { new Product { Name = "P1", Price = 100 } };
+                }
+            }
+            public void AddProduct(Product p) { }
+        }
+
+        [Fact]
+        public void RepositoryPropertyCalledOnce()
+        {
+            var mock = new Mock<IRepository>();
+            mock.SetupGet(m => m.Products)
+                .Returns(new[] { new Product { Name = "P1", Price = 100 } });
+            var controller = new HomeController { Repository = mock.Object };
+
+            var result = controller.Index();
+
+            //Assert.Equal(1, repo.PropertyCounter);
+            mock.VerifyGet(m => m.Products, Times.Once);
         }
     }
 }
