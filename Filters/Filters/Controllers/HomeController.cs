a68f2b5421c5b0c1f89ff3bb43c7fd3b0db3e7b0
Oleksii <31726540+default51400@users.noreply.github.com>
2019-07-01 00:14:20 +0300

    work with custom authFilter https
    Infrastructure/[HttpsOnly]

---
+++ Filters/Filters/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Filters.Infrastructure;
+
+namespace Filters.Controllers
+{
+    [HttpsOnly]
+    public class HomeController : Controller
+    {
+        public ViewResult Index() => View("Message",
+            "This is the Index action on the Ноmе controller");
+
+        public ViewResult SecondAction() => View("Message",
+            "This is the SecondAction on the Ноmе controller");
+    }
+}
